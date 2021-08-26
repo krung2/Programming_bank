e7f84beeadf42eb4e483d0cb1588b696b1e5586a
krung2 <jungbin4337@dgsw.hs.kr>
2021-08-26 17:11:22 +0900

    Add: env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { validate } from './config/env/env.validation';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      cache: true,
+      validate,
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+
+export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, { cors: false });
+  const port: number = app.get(ConfigService).get('PORT');
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(port);
 }
+
 bootstrap();
