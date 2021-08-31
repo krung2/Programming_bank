c025ec73d269ecda9b2ccad5cd166199d3ca2727
krung2 <jungbin4337@dgsw.hs.kr>
2021-08-31 20:42:42 +0900

    Fix: userRepository

--- src/user/repositories/user.repository.ts
+++ src/user/repositories/user.repository.ts
@@ -13,7 +13,7 @@ export default class UserRepository extends Repository<User> {
   public findById(id: string, pw: string): Promise<User | undefined> {
     return this.createQueryBuilder()
       .where('id = :id', { id })
-      .where('pw = :pw', { pw })
+      .andWhere('pw = :pw', { pw })
       .getOne();
   }
 }
