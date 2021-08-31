b36374b72b5dcf9e46856d1b2263c3fdc60a8e15
krung2 <jungbin4337@dgsw.hs.kr>
2021-08-31 19:46:58 +0900

    Fix: Container

--- src/token/token.module.ts
+++ src/token/token.module.ts
@@ -1,15 +1,13 @@
+import 'dotenv/config';
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
-import { Container } from 'typeorm-typedi-extensions';
 import { TokenController } from './token.controller';
 import { TokenService } from './token.service';
 
-const a: ConfigService = Container.get(ConfigService);
-
 @Module({
   imports: [
-    JwtModule.register({ secret: a.get('JWT_SECRET') }),
+    JwtModule.register({ secret: process.env.JWT_SECRET }),
     ConfigModule
   ],
   controllers: [TokenController],
