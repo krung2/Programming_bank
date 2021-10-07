f88df3f52e7886e9703fac7a8a87bcddf79b109b
krung2 <jungbin4337@dgsw.hs.kr>
2021-10-07 10:47:48 +0900

    Create send.subscriber.ts

---
+++ src/apis/remittance/send.subscriber.ts
@@ -0,0 +1,28 @@
+import { Injectable } from "@nestjs/common";
+import { SseConst } from "src/global/constants/Sse.const";
+import { Connection, EntitySubscriberInterface, EventSubscriber, InsertEvent } from "typeorm";
+import { SseService } from "../sse/sse.service";
+import Send from "./entities/send.entity";
+
+@Injectable()
+@EventSubscriber()
+export class SendSubscriber implements EntitySubscriberInterface<Send> {
+
+  constructor(
+    readonly connection: Connection,
+    private readonly sseService: SseService,
+  ) {
+    connection.subscribers.push(this);
+  }
+
+  listenTo() {
+    return Send;
+  }
+
+  beforeInsert(event: InsertEvent<Send>) {
+    this.sseService.addEvent({
+      type: SseConst.SEND,
+      data: event.entity
+    });
+  }
+}
