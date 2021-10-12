6ca9c417fe381c57cedd58a7da1a1630ab0dd901
krung2 <jungbin4337@dgsw.hs.kr>
2021-10-12 11:54:26 +0900

    ADD sse connect in error

---
+++ public/index.js
@@ -0,0 +1,14 @@
+const errorSse = new EventSource('/sse');
+const errorBox = document.getElementById('error-box');
+errorSse.addEventListener('error', ({ data }) => {
+
+  const parsedData = JSON.parse(data);
+
+  const error = document.createElement('error-message');
+  error.setAttribute('status', parsedData.status);
+  error.setAttribute('ip', parsedData.sendIp);
+  error.setAttribute('url', parsedData.sendUrl);
+  error.setAttribute('content', parsedData.message);
+
+  errorBox.appendChild(error);
+})
