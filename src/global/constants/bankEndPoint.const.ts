bcd935ac5e1212ce35bad500c7f809611f859487
krung2 <jungbin4337@dgsw.hs.kr>
2021-10-15 17:19:24 +0900

    Fix: server port

--- src/global/constants/bankEndPoint.const.ts
+++ src/global/constants/bankEndPoint.const.ts
@@ -1,9 +1,9 @@
 export enum BankEndPoint {
-  JB = 'http://localhost:8081/remittance/receive',
+  JB = 'http://localhost:8080/remittance/receive',
   HY = 'http://34.64.79.162:8080/transfer/get',
   JM = '',
   MG = '',
-  GJB = 'http://localhost:8081/account/find/id/',
+  GJB = 'http://localhost:8080/account/find/id/',
   GHY = 'http://34.64.79.162:8080/transfet/send/',
   GJM = '',
   GMG = '',
